60cc5c9990af6fa9ad9fa75b5f09da62ffb2d295
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-07-20 23:25:23 -0500

    citas endpoints

--- database/database.js
+++ database/database.js
@@ -2,7 +2,15 @@ import {Sequelize} from "sequelize";
 
 // process.env.DATABASE_URL = 'postgres://postgres:admin@localhost/db-proyecto';
 
-export const sequelize = new Sequelize("ejemplodb","postgres","nico",{
-    host: "localhost", //aca iria el ip de la otra maquina,
-    dialect: "postgres"
-})
+const options = {
+    dialectOptions: {
+        ssl: {
+            require: false,
+            rejectUnauthorized: false
+        }
+    }
+};
+
+export const sequelize = new Sequelize(
+    process.env.DATABASE_URL
+)
